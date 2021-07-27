0832aac5336be3eaabce4257925386044b9e1475
kaymarshall <keith.marshall@pgcps.org>
2021-07-27 16:13:42 -0400

    Added email, but it didn't work

--- app/src/main/java/com/example/quizapp2021/Question.java
+++ app/src/main/java/com/example/quizapp2021/Question.java
@@ -41,4 +41,9 @@ public class Question {
                 ", correctAnswer=" + correctAnswer +
                 '}';
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/quizapp2021/ScoreActivity.java
+++ app/src/main/java/com/example/quizapp2021/ScoreActivity.java
@@ -16,6 +16,7 @@ public class ScoreActivity extends AppCompatActivity {
     TextView scoreTV;
     int score;
     Intent incomingIntent;
+    Button emailScore;
 
 
     @Override
@@ -23,24 +24,29 @@ public class ScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
 
-
         scoreTV = (TextView) findViewById(R.id.scoreTV);
 
+        emailScore = (Button) findViewById(R.id.emailButton);
         incomingIntent = getIntent();
         score = incomingIntent.getIntExtra("scoreName", 0);
         scoreTV.setText("Your score is: " + score);
 
+        emailScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] addresses = new String[]{"keith.marshall@pgcps.org"};
+                String subject = "Here is your score:";
+                composeEmail(addresses, subject);
+            }
+        });
     }
-
     public void composeEmail(String[] addresses, String subject) {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_EMAIL, addresses);
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+
         if (intent.resolveActivity(getPackageManager()) != null) startActivity(intent);
     }  //Source: https://developer.android.com/guide/components/intents-common#Email
-
-
-
 
 }
